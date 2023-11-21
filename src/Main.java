1ad7ba7eef1b0d5b03d9dc07b21221ab3df771e5
maxi <maxi.ellnestam@gmail.com>
2023-11-21 20:19:25 +0100

    Add Owner name logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Owner owner = new Owner("MAXIMILIAN eLLENSTAM");
+        Owner owner2 = new Owner("PER-OLoF ÖsTeRÅkeR");
+
+        System.out.println(owner.getName());
+        System.out.println(owner2);
     }
 }
--- src/Owner.java
+++ src/Owner.java
@@ -1,2 +1,43 @@
+//Maximilian Ellnestam mael0424
 public class Owner {
+    private final String name;
+
+    public Owner(String name){
+        this.name = formatName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    private static String formatName(String name){
+        String[] names = name.toLowerCase().split(" ");
+        StringBuilder formattedName = new StringBuilder();
+
+        for (int i = 0; i < names.length; i++){
+            if (names[i].contains("-")){
+                int secondNameStart = names[i].indexOf("-") + 1;
+                String firstName = names[i].substring(0, secondNameStart);
+                String firstCharSecondName = names[i].substring(secondNameStart, secondNameStart + 1).toUpperCase();
+                String secondName =  names[i].substring(secondNameStart + 1);
+
+                names[i] = firstName + firstCharSecondName + secondName;
+            }
+
+            names[i] = names[i].substring(0, 1).toUpperCase() + names[i].substring(1);
+        }
+
+        for (String splitName : names){
+            formattedName.append(splitName);
+            if (!splitName.equals(names[names.length - 1]))
+                formattedName.append(" ");
+        }
+
+        return formattedName.toString();
+    }
 }
