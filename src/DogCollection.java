a6088e906b157255c37fbabb33b308200d6951cc
maxi <maxi.ellnestam@gmail.com>
2023-11-22 16:18:09 +0100

    Add DogCollection

---
+++ src/DogCollection.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Optional;
+
+//Maximilian Ellnestam mael0424
+public class DogCollection {
+
+    private ArrayList<Dog> dogs = new ArrayList<>();
+
+    public boolean addDog(Dog dog){
+        if (containsDog(dog.getName()))
+            return false;
+        return dogs.add(dog);
+    }
+
+    public boolean removeDog(Dog dog){
+        return dogs.remove(dog);
+    }
+    public boolean removeDog(String name){
+        Optional<Dog> dog = dogs.stream().filter(x -> x.getName().equals(name)).findFirst();
+        if (!containsDog(name) || dog.isEmpty())
+            return false;
+        return dogs.remove(dog.get());
+    }
+
+    public boolean containsDog(String name){
+        return dogs.stream().anyMatch(x -> x.getName().equals(name));
+    }
+
+    public boolean containsDog(Dog dog){
+        return dogs.contains(dog);
+    }
+
+    public ArrayList<Dog> getDogs() {
+        DogSorter.sortDogs(new DogTailNameComparator(), dogs);
+        return (ArrayList<Dog>) dogs.clone();
+    }
+
+    public Dog getDog(String name){
+        Optional<Dog> dog = dogs.stream().filter(x -> x.getName().equals(name)).findFirst();
+        if (!containsDog(name) || dog.isEmpty())
+            return null;
+        return dog.get();
+    }
+
+    public ArrayList<Dog> getDogsWithMinimumTailLength(double d){
+        ArrayList<Dog> dogArrayList = new ArrayList<>(dogs.stream().filter(x -> x.getTailLength() >= d).toList());
+        DogSorter.sortDogs(new DogTailNameComparator(), dogArrayList);
+        return dogArrayList;
+    }
+}
